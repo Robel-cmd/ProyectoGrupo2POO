e4ebacc066f792dabee0df5dab1a49c6b7dd68b1
Robel-cmd <robelisaisalgado3@gmail.com>
2025-11-09 20:50:57 -0600

    Primer commit

---
+++ src/modeloEntidades/ModeloUsuario.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeloEntidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hanzo
+ */
+public class ModeloUsuario {
+    // atributos
+    private int id;
+    private String nombress;
+    private String apellidos;
+    private String genero;
+    private int dni;
+    private int telefono;
+    private int correo;
+    private String contraseña;
+    private int rol;
+   
+    
+    public void registrarCliente(){
+        
+    }
+    public void eliminarCliente(){
+        
+    }
+    public void actualizarCliente(){
+        
+    }
+    
+}
---
+++ src/modeloEntidades/modeloCliente.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeloEntidades;
+
+/**
+ *
+ * @author hanzo
+ */
+public class modeloCliente {
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private int dni;
+    private int telefono;
+    private int correo;
+    
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public int getCorreo() {
+        return correo;
+    }
+    
+    // setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(int correo) {
+        this.correo = correo;
+    }
+    
+    
+
+    
+}
